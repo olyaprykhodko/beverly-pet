550caec1cca8fcf8c1670171e09bf5ee1ef965d2
Olha Prykhodko <o.u.prihodko@gmail.com>
2023-12-27 22:33:54 +0200

    products-list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">E-shop</a>
+        <a href="/">Beverly</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <div className="productImage">
+                  <img src={product.image} alt={product.name} />
+                </div>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>€{product.price}</strong>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Erborian BB-Cream',
+      slug: 'erborian-bb-cream',
+      category: 'Foundation',
+      image: '/images/erborian-bb.jpg',
+      price: 40,
+      countInStock: 20,
+      brand: 'Erborian',
+      rating: 4.9,
+      numReviews: 14,
+      description:
+        'The Erborian BB cream works as a light coverage foundation and evens out your skin tone while covering imperfections. It also contains a highly concentrated mix of herbs that promote youthfulness and provide all the benefits of regenerating skin care',
+    },
+    {
+      name: 'Gosh Eyedentity',
+      slug: 'gosh-eyedentity',
+      category: 'Palettes',
+      image: '/images/gosh-palette.jpg',
+      price: 6,
+      countInStock: 33,
+      brand: 'Gosh',
+      rating: 4.8,
+      numReviews: 9,
+      description:
+        'Gosh Eyedentity eyeshadow palette lets you choose from 9 perfectly matched shades. They are intensely pigmented and easy to combine with each other and give your makeup both matt and metallic finishes.',
+    },
+    {
+      name: 'Holika Holika Watermelon Mask',
+      slug: 'holika-holika-watermelon-mask',
+      category: 'Skin Care',
+      image: '/images/holika-holika-mask.jpg',
+      price: 19,
+      countInStock: 18,
+      brand: 'Holika Holika',
+      rating: 4.6,
+      numReviews: 13,
+      description:
+        'The intense night Holika Holika Watermelon Mask is perfect for quick regeneration of dry and dehydrated skin, thanks to the 95% watermelon extract. The lycopene complex stimulates circulation, provides excellent antioxidant effects and keeps skin cells young. Wake up even more beautiful.',
+    },
+    {
+      name: 'Kérastase Chronologiste Bain Régénérant',
+      slug: 'kérastase-chronologiste-bain-régénérant',
+      category: 'Hair',
+      image: '/images/kerastase-shampoo.jpg',
+      price: 23,
+      countInStock: 26,
+      brand: 'Kérastase',
+      rating: 5,
+      numReviews: 16,
+      description:
+        'The Kérastase Chronologiste Bain Régénérant strengthening and revitalising shampoo has a characteristic scent that will invigorate both your hair and your soul. It washes away impurities while providing long-lasting hydration, shine and strength from roots to ends.',
+    },
+  ],
+};
+
+export default data;
